44f977761b723ba58092ba88bc508eb8af3ccc6b
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-05-15 15:11:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PreciousStone import PreciousStone
 
 if __name__ == '__main__':
-    stones = [PreciousStone.PreciousStone() for _ in range(2)]
-    stones[1] = PreciousStone.PreciousStone("Diamond", 2.356, "Blue", 0.72, 29.2)
+    stones = [PreciousStone() for _ in range(2)]
+    stones[1] = PreciousStone("Diamond", 2.356, "Blue", 0.72, 29.2)
     print(stones[1].get_total_price())
     print(stones[1].increase_clarity())
     print(stones[1].increase_price(20))
