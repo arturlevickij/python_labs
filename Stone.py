09e5a1ccb28d8cf9094121df708f2ff894990d60
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-05-15 15:09:00 +0300

    Rework Stone.py

--- Stone.py
+++ Stone.py
@@ -1,5 +1,7 @@
 class PreciousStone:
-    def __init__(self, name, carat, color, clarity, pricepercarat):
+    __instance = None
+
+    def __init__(self, name="", carat=0.0, color="", clarity=0.0, price_per_carat=0.0):
         """
             Parameters:
             name (str): The unique name of the stone.
@@ -12,22 +14,25 @@ class PreciousStone:
         self.carat = carat
         self.color = color
         self.clarity = clarity
-        self.pricePerCarat = pricepercarat
-
+        self.price_per_carat = price_per_carat
 
     def __str__(self):
         return f'Name: {self.name} , Carat: {self.carat}, Color: {self.color}, Clarity: {self.clarity} ' \
-           f', Price per Carat: {self.pricePerCarat}'
+               f', Price per Carat: {self.price_per_carat}'
 
+    @staticmethod
+    def get_instance():
+        if PreciousStone.__instance is None:
+            PreciousStone.__instance = PreciousStone()
+        return PreciousStone.__instance
 
     def increase_clarity(self):
         self.clarity = self.clarity + 1
         return self.clarity
 
-
     def get_total_price(self):
-        return self.pricePerCarat * self.carat
-
+        return self.price_per_carat * self.carat
 
     def increase_price(self, percentage):
         return self.get_total_price() * percentage / 100 + self.get_total_price()
+
