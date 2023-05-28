4c6d1d5c7c09e047aae95c5236c4774054d9f67f
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-05-28 17:59:15 +0300

    Update PreciousStone.py

--- models/PreciousStone.py
+++ models/PreciousStone.py
@@ -1,27 +1,52 @@
+"""
+This module provides a Stone class for managing stones.
+"""
 from models.stone import Stone
 
 
 class PreciousStone(Stone):
-    def __init__(self, name="", carat=0.0, color="", clarity=0.0, price_per_carat=0.0):
+    """
+        Class representing a Precious stone.
+
+        This class inherits from the Stone class and provides
+        specific implementation for a precious stone.
+
+        Attributes:
+            name (str): The unique name of the stone.
+            color (str): The color of the stone.
+            carat (float): The carat of the stone.
+            clarity (float): The clarity of the stone.
+            weight_in_gram (float): The weight of the stone in grams.
+            price_per_gram (float): The price per gram of the stone.
         """
-        Parameters:
-        name (str): The unique name of the stone.
-        carat (float): The carat of the stone.
-        color (str): The color of the stone.
-        clarity (float): The clarity of the stone.
-        price_per_carat (float): The price per carat of the stone.
-        """
+
+    # pylint: disable=too-many-arguments
+    def __init__(self, name="", color="", carat=0.0, clarity=0.0,
+                 price_per_gram=0.0, weight_in_gram=0):
         super().__init__(name, color)
         self.carat = carat
         self.clarity = clarity
-        self.price_per_carat = price_per_carat
-
-    def __str__(self):
-        return f'Name: {self.name}, Carat: {self.carat}, Color: {self.color}, Clarity: {self.clarity}, ' \
-               f'Price per Carat: {self.price_per_carat}'
+        self.price_per_gram = price_per_gram
+        self.weight_in_gram = weight_in_gram
 
     def get_total_price(self):
-        return self.price_per_carat * self.carat
+        """
+            Returns a stones total price.
+        """
+        return self.price_per_gram * self.carat
 
     def increase_price(self, percentage):
+        """
+            Returns a stones increase price.
+        """
         return self.get_total_price() * percentage / 100 + self.get_total_price()
+
+    def get_full_price(self):
+        """
+            Returns a stones full price.
+        """
+        return self.price_per_gram * self.weight_in_gram
+
+    def __str__(self):
+        return f'{super().__str__()}, Carat: {self.carat} Clarity: {self.clarity}, ' \
+               f'Price per carat: {self.price_per_gram}, Weight in gram: {self.weight_in_gram}'
