8f27697ceb38c9562ed7eff0ce881d94f7193a13
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-05-28 18:00:09 +0300

    Update stone.py

--- models/stone.py
+++ models/stone.py
@@ -1,6 +1,21 @@
+"""
+The module that contains the Stone class is an abstract class that represents a stone.
+"""
 from abc import ABC, abstractmethod
 
+
 class Stone(ABC):
+    """
+        Abstract base class representing a stone.
+
+        This class defines the common attributes and methods that are
+        expected to be implemented by concrete stones
+
+        Attributes:
+        name (str): The name of the stone.
+        color (str): The color of the stone.
+        """
+
     def __init__(self, name="", color=""):
         """
         Parameters:
@@ -9,11 +24,19 @@ class Stone(ABC):
         """
         self.name = name
         self.color = color
-        
+
+
     @abstractmethod
     def get_full_price(self):
         """
         Abstract method to get the full price of the stone.
-        This method must be implemented by the subclasses.
         """
+
+    def do_something(self):
         pass
+
+    def get_attributes_by_type(self, data_type):
+        return {key: value for key, value in self.__dict__.items() if isinstance(value, data_type)}
+
+    def __str__(self):
+        return f"Name - {self.name}, Color = {self.color}"
