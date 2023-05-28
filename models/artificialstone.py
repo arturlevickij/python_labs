507392bb62267d2c77292eecc4bec35d23d416d7
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-05-28 17:59:29 +0300

    Update artificialstone.py

--- models/artificialstone.py
+++ models/artificialstone.py
@@ -1,29 +1,38 @@
+"""
+This module provides a Stone class for managing stones.
+"""
 from models.stone import Stone
 
 
 class ArtificialPreciousStone(Stone):
-    def __init__(self, name="", carat=0.0, color="", clarity=0.0, price_per_carat=0.0,
-                 lab_name="", weight_grams=0.0, price_per_gram=0.0):
-        """
-        Parameters:
+    """
+    Class representing an Artificial precious stone.
+
+    This class inherits from the Stone class and provides specific implementation for an
+    artificial precious stone.
+
+    Attributes:
         name (str): The unique name of the stone.
-        carat (float): The carat of the stone.
         color (str): The color of the stone.
-        clarity (float): The clarity of the stone.
-        price_per_carat (float): The price per carat of the stone.
         lab_name (str): The name of the laboratory.
-        weight_grams (float): The weight of the stone in grams.
+        weight_in_gram (float): The weight of the stone in grams.
         price_per_gram (float): The price per gram of the stone.
-        """
-        super().__init__(name, carat, color, clarity, price_per_carat)
+    """
+
+    # pylint: disable=too-many-arguments
+    def __init__(self, name="", color="", lab_name="",
+                 weight_in_gram=0.0, price_per_gram=0.0):
+        super().__init__(name, color)
         self.lab_name = lab_name
-        self.weight_grams = weight_grams
+        self.weight_in_gram = weight_in_gram
         self.price_per_gram = price_per_gram
 
-    def __str__(self):
-        return f'Name: {self.name}, Carat: {self.carat}, Color: {self.color}, Clarity: {self.clarity}, ' \
-               f'Price per Carat: {self.price_per_carat}, Lab Name: {self.lab_name}, ' \
-               f'Weight (grams): {self.weight_grams}, Price per Gram: {self.price_per_gram}'
-
     def get_full_price(self):
-        return self.price_per_gram * self.weight_grams
+        """
+            Returns a list of sports supported by the skating rink.
+        """
+        return self.price_per_gram * self.weight_in_gram
+
+    def __str__(self):
+        return f'{super().__str__()}, Lab Name: {self.lab_name}' \
+               f'Weight (grams): {self.weight_in_gram}, Price per Gram: {self.price_per_gram}'
