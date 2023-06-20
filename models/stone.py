1de9264f73e12698fd4206952e66032698f7fbb8
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-06-20 17:05:23 +0300

    Update stone.py

--- models/stone.py
+++ models/stone.py
@@ -16,7 +16,8 @@ class Stone(ABC):
         color (str): The color of the stone.
         """
 
-    def __init__(self, name="", color=""):
+
+    def __init__(self, name="", color="", weight=0):
         """
         Parameters:
         name (str): The name of the stone.
@@ -24,7 +25,8 @@ class Stone(ABC):
         """
         self.name = name
         self.color = color
-
+        self.weight = weight
+        self.data_set = set()
 
     @abstractmethod
     def get_full_price(self):
@@ -32,11 +34,17 @@ class Stone(ABC):
         Abstract method to get the full price of the stone.
         """
 
-    def do_something(self):
-        pass
-
     def get_attributes_by_type(self, data_type):
         return {key: value for key, value in self.__dict__.items() if isinstance(value, data_type)}
 
     def __str__(self):
-        return f"Name - {self.name}, Color = {self.color}"
+        return f"Name - {self.name}, Color = {self.color}, Weight = {self.weight}"
+
+    def __iter__(self):
+        """
+        Iterate over the data_set.
+
+        Returns:
+            iter: An iterator over the data_set.
+        """
+        return iter(self.data_set)
